5bf8b79c5dafa6653af74a40e0cd316a931ba808
Nehemías Lione <114757343+NejeCFO@users.noreply.github.com>
2025-02-13 16:16:41 -0300

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
+  reporter: process.env.CI ? 'dot' : [
     ['list', {printSteps: true}],
     ['html', {outputFolder: './src/reports', open: 'never'}], 
     ['json', { outputFile: './src/reports/report.json' }]],
