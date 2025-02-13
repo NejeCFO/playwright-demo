778143bdaf213d9e1bf57b2c34239230ef596217
Nehemías Lione <114757343+NejeCFO@users.noreply.github.com>
2025-02-13 14:02:02 -0300

    config para reports html y json

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html', {outputFolder: './src/reports'}], 
+    ['json', { outputFile: './src/reports/report.json' }]],
   timeout: 120_000,
   expect: {
     timeout: 70_000,
